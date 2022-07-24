1dc2ba49dd8176987022b6125733aa071371eee3
mnsuhairil <2020897584@student.uitm.edu.my>
2022-07-24 21:33:48 +0800

    updated website nasa furnature
    completed

---
+++ NasaFurnitureWebsite/footer.php
@@ -0,0 +1,295 @@
+<?php
+$statement = $pdo->prepare("SELECT * FROM tbl_settings WHERE id=1");
+$statement->execute();
+$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+foreach ($result as $row)
+{
+	$footer_about = $row['footer_about'];
+	$contact_email = $row['contact_email'];
+	$contact_phone = $row['contact_phone'];
+	$contact_address = $row['contact_address'];
+	$footer_copyright = $row['footer_copyright'];
+	$total_recent_post_footer = $row['total_recent_post_footer'];
+    $total_popular_post_footer = $row['total_popular_post_footer'];
+    $newsletter_on_off = $row['newsletter_on_off'];
+    $before_body = $row['before_body'];
+}
+?>
+
+
+
+
+
+<!-- footer section starts  -->
+  <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+	
+<section class="footer" align:center;>
+<style>
+h1 {text-align: center;}
+p {text-align: center;}
+div {text-align: center;}
+</style>
+    <div class="box-container">
+
+        <div class="box">
+            <h3>NasaFurniture</h3>
+            <p>Nasa’s Furniture is a company that provides second hand furniture at  their  store.  The
+				owner of this company was Mr. Nasaruddin Muhammad, and were established in Novenber 2019. 
+				Their headquarters are located in Machang,kelantan. This company has 3 franchises . They 
+				started their operation on november 2019 and their founder is Mr nasarudin. This company
+				promises their customers to give their best services. They  also  provide  COD  services
+				Cash On Delivery).</p>
+            <div class="share">
+                <a target="_blank" href="#" class="fab fa-facebook-f"> </a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <h3>Contact Info</h3>
+            <a href="#" class="links"> <i class="fas fa-phone"></i> +01114127143 </a>
+            <a href="#" class="links"> <i class="fas fa-phone"></i> +01113214432 </a>
+            <a href="#" class="links"> <i class="fas fa-envelope"></i> NasaFurnatureCo@gmail.com </a>
+            <a href="#" class="links"> <i class="fas fa-map-marker-alt"></i> Kuala Terengganu, Terengganu - 876877 </a>
+        </div>
+
+        <div class="box">
+            <h3>Quick Links</h3>
+            <a href="#" class="links"> <i class="fas fa-arrow-right"></i> home </a>
+            <a href="#" class="links"> <i class="fas fa-arrow-right"></i> features </a>
+            <a href="#" class="links"> <i class="fas fa-arrow-right"></i> products </a>
+            <a href="#" class="links"> <i class="fas fa-arrow-right"></i> categories </a>
+            <a href="#" class="links"> <i class="fas fa-arrow-right"></i> services </a>
+            <a href="#" class="links"> <i class="fas fa-arrow-right"></i> about </a>
+        </div>
+
+
+    </div>
+
+    <div class="credit"> created by <span> NasaFurniture Company </span> | all rights reserved </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+<div class="footer-bottom">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 copyright">
+				<?php echo $footer_copyright; ?>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<a href="#" class="scrollup">
+	<i class="fa fa-angle-up"></i>
+</a>
+
+<?php
+$statement = $pdo->prepare("SELECT * FROM tbl_settings WHERE id=1");
+$statement->execute();
+$result = $statement->fetchAll(PDO::FETCH_ASSOC);                            
+foreach ($result as $row) {
+    $stripe_public_key = $row['stripe_public_key'];
+    $stripe_secret_key = $row['stripe_secret_key'];
+}
+?>
+
+<script src="assets/js/jquery-2.2.4.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="https://js.stripe.com/v2/"></script>
+<script src="assets/js/megamenu.js"></script>
+<script src="assets/js/owl.carousel.min.js"></script>
+<script src="assets/js/owl.animate.js"></script>
+<script src="assets/js/jquery.bxslider.min.js"></script>
+<script src="assets/js/jquery.magnific-popup.min.js"></script>
+<script src="assets/js/rating.js"></script>
+<script src="assets/js/jquery.touchSwipe.min.js"></script>
+<script src="assets/js/bootstrap-touch-slider.js"></script>
+<script src="assets/js/select2.full.min.js"></script>
+<script src="assets/js/custom.js"></script>
+
+<style>
+
+.footer{
+    background: #232f3e;
+}
+
+.footer .box-container{
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(25rem, 1fr));
+    gap:1.5rem;
+}
+
+.footer .box-container .box h3{
+    font-size: 2.5rem;
+	color:#eee;
+    padding:1rem 0;
+}
+
+.footer .box-container .box h3 i{
+    color: #fff;
+}
+
+.footer .box-container .box .links{
+    display: block;
+    font-size: 1.5rem;
+	color:#eee;
+    padding:1rem 0;
+}
+
+.footer .box-container .box .links i{
+    color:var(--orange);
+    padding-right: .5rem;
+}
+
+.footer .box-container .box .links:hover i{
+    padding-right: 2rem;
+}
+
+
+.footer .box-container .box p{
+    line-height: 1.8;
+    font-size: 1.5rem;
+    color:#eee;
+    padding:1rem 0;
+}
+
+.footer .box-container .box .share a{
+    height:4rem;
+    width:4rem;
+    line-height:4rem;
+    border-radius: .5rem;
+    font-size: 2rem;
+    color:var(--black);
+    margin-right: .2rem;
+    background: #eee;
+    text-align: center;
+}
+
+.footer .box-container .box .share a:hover{
+    background: var(--orange);
+    color: #fff;
+}
+
+.footer .box-container .box .email{
+    width: 100%;
+    margin:.7rem 0;
+    padding:1rem;
+    border-radius: .5rem;
+    background: #eee;
+    font-size: 1.6rem;
+    color:var(--black);
+    text-transform: none;
+}
+
+.footer .box-container .box .payment-img{
+    margin-top: 2rem;
+    height: 3rem;
+    display: block;
+}
+
+.footer .credit{
+    text-align: center;
+    margin-top: 2rem;
+    padding:1rem;
+    padding-top: 2.5rem;
+    font-size: 2rem;
+    color:var(--black);
+    border-top: var(--border);
+}
+
+.footer .credit span{
+    color:var(--orange);
+}
+
+
+
+
+@media (max-width:450px){
+
+    .footer{
+        text-align: center;
+    }
+
+    .footer .box-container .box .payment-img{
+        margin: 2rem auto;
+    }
+
+}
+</style>
+<script>
+	function confirmDelete()
+	{
+	    return confirm("Sure you want to delete this data?");
+	}
+	$(document).ready(function () {
+		advFieldsStatus = $('#advFieldsStatus').val();
+
+		$('#paypal_form').hide();
+		$('#stripe_form').hide();
+		$('#bank_form').hide();
+
+        $('#advFieldsStatus').on('change',function() {
+            advFieldsStatus = $('#advFieldsStatus').val();
+            if ( advFieldsStatus == '' ) {
+            	$('#paypal_form').hide();
+				$('#stripe_form').hide();
+				$('#bank_form').hide();
+            } else if ( advFieldsStatus == 'PayPal' ) {
+               	$('#paypal_form').show();
+				$('#stripe_form').hide();
+				$('#bank_form').hide();
+            } else if ( advFieldsStatus == 'Stripe' ) {
+               	$('#paypal_form').hide();
+				$('#stripe_form').show();
+				$('#bank_form').hide();
+            } else if ( advFieldsStatus == 'Bank Deposit' ) {
+            	$('#paypal_form').hide();
+				$('#stripe_form').hide();
+				$('#bank_form').show();
+            }
+        });
+	});
+
+
+	$(document).on('submit', '#stripe_form', function () {
+        // createToken returns immediately - the supplied callback submits the form if there are no errors
+        $('#submit-button').prop("disabled", true);
+        $("#msg-container").hide();
+        Stripe.card.createToken({
+            number: $('.card-number').val(),
+            cvc: $('.card-cvc').val(),
+            exp_month: $('.card-expiry-month').val(),
+            exp_year: $('.card-expiry-year').val()
+            // name: $('.card-holder-name').val()
+        }, stripeResponseHandler);
+        return false;
+    });
+    Stripe.setPublishableKey('<?php echo $stripe_public_key; ?>');
+    function stripeResponseHandler(status, response) {
+        if (response.error) {
+            $('#submit-button').prop("disabled", false);
+            $("#msg-container").html('<div style="color: red;border: 1px solid;margin: 10px 0px;padding: 5px;"><strong>Error:</strong> ' + response.error.message + '</div>');
+            $("#msg-container").show();
+        } else {
+            var form$ = $("#stripe_form");
+            var token = response['id'];
+            form$.append("<input type='hidden' name='stripeToken' value='" + token + "' />");
+            form$.get(0).submit();
+        }
+    }
+</script>
+<?php echo $before_body; ?>
+</body>
+</html>
